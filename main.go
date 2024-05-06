cc9d5bfed25ae5e3742cf04af998f53fcbc0b4e9
hermandev <hermantolakoro@gmail.com>
2024-05-06 22:03:52 +0800

    wip: add OpenBroeserV2 for queue user login

--- main.go
+++ main.go
@@ -47,7 +47,7 @@ func main() {
 		users = append(users, user)
 	}
 
-	OpenBrowser(users)
+	OpenBrowserV2(users)
 }
 
 func OpenBrowser(data []User) {
@@ -107,4 +107,75 @@ func OpenBrowser(data []User) {
 func runTask(cdp chromedp.QueryAction, str string) chromedp.QueryAction {
 	fmt.Println(str)
 	return cdp
+}
+
+func OpenBrowserV2(data []User) {
+	var wg sync.WaitGroup
+
+	// Channel untuk mengontrol proses menjalankan browser secara berurutan
+	done := make(chan bool)
+
+	for _, user := range data {
+		wg.Add(1)
+
+		go func(user User) {
+			defer wg.Done()
+
+			fmt.Printf("Email: %s\n", user.Email)
+			opts := append(chromedp.DefaultExecAllocatorOptions[:],
+				chromedp.DisableGPU,
+				chromedp.UserDataDir("tmp/"+user.Email),
+				chromedp.Flag("headless", false),
+				chromedp.Flag("enable-automation", false),
+				chromedp.Flag("incognito", true),
+				chromedp.Flag("restore-on-startup", false),
+			)
+
+			allocCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
+
+			// Buat context baru untuk setiap goroutine
+			ctx, cancel := chromedp.NewContext(allocCtx)
+
+			// Buat context baru untuk setiap goroutine
+			// ctx, cancel := chromedp.NewContext(context.Background())
+			defer cancel()
+
+			// Jalankan tugas-tugas chromedp dalam goroutine
+			if err := chromedp.Run(ctx, chromedp.Tasks{
+				runTask(chromedp.Navigate("https://accounts.google.com/"), "Open Google Account"),
+				runTask(chromedp.WaitVisible("#identifierId", chromedp.ByID), "Check Input Email"),
+				runTask(chromedp.SendKeys("#identifierId", user.Email, chromedp.ByID), "Input Email"),
+				runTask(chromedp.Click("#identifierNext", chromedp.ByID), "Next"),
+				runTask(chromedp.WaitVisible("//*[@id='password']/div[1]/div/div[1]/input", chromedp.BySearch), "Check Input Password"),
+				runTask(chromedp.SendKeys("//*[@id='password']/div[1]/div/div[1]/input", user.Password, chromedp.BySearch), "Input Password"),
+				runTask(chromedp.Click("/html/body/div[1]/div[1]/div[2]/c-wiz/div/div[3]/div/div[1]/div/div/button", chromedp.BySearch), "Next Login"),
+				// aktifkan comment ini jika meminta confirm
+				// runTask(chromedp.WaitVisible("//*[@id='tos_form']", chromedp.BySearch), "Wait Confirm "),
+				// runTask(chromedp.Submit("//*[@id='tos_form']", chromedp.BySearch), "Submit Confirm"),
+				// chromedp.WaitNotVisible("//*[@id='tos_form']", chromedp.BySearch),
+				runTask(chromedp.WaitVisible("/html/body/div[4]/header", chromedp.BySearch), "Wait Profile"),
+				runTask(chromedp.Navigate("http://tiny.cc/jokoganteng"), "Open Cloud Console"),
+				runTask(chromedp.WaitVisible("/html/body/div/div[2]/div", chromedp.BySearch), "Wait Modal"),
+				runTask(chromedp.Click("//*[@id='mat-mdc-checkbox-1-input']", chromedp.BySearch), "Checklis Modal"),
+				runTask(chromedp.Click("/html/body/div/div[2]/div/mat-dialog-container/dialog-overlay/div[5]/modal-action/button", chromedp.BySearch), "Config Modal"),
+				runTask(chromedp.WaitNotVisible("//*[@id='cloudshell']/div/loading-screen/div/div/div[1]/div", chromedp.BySearch), "Wait Close Modal"),
+				runTask(chromedp.Click("document.querySelector('#cloudshell > standalone-header > div > mat-toolbar > span > cloudshell-view-controls > visibility-toggle:nth-child(2)')", chromedp.ByJSPath), "Click Button Terminal"),
+			}); err != nil {
+				done <- true
+				// log.Fatalln(err)
+			}
+
+			// Tambahkan delay setelah setiap iterasi
+			time.Sleep(time.Minute)
+			done <- true
+		}(user)
+
+		<-done
+	}
+
+	// Tunggu semua goroutine selesai
+	wg.Wait()
+
+	// Tutup channel done untuk memberi tahu bahwa semua proses sudah selesai
+	close(done)
 }
